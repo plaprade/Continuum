61c1ac4acf10cbd54ea0352458a65e04811f9f6c
ciphermonk <ciphermonk@altera-vita.com>
2012-12-14 16:03:49 +0700

    Initial commit

---
+++ lib/CondVar/Util.pm
@@ -0,0 +1,197 @@
+package CondVar::Util;
+
+use strict;
+use warnings;
+
+use AnyEvent;
+
+require Exporter;
+
+our @ISA = qw( Exporter );
+
+our %EXPORT_TAGS = ( all => [ qw(
+    cv
+    cv_timer
+    then
+    with
+    cv_and
+    cv_or
+    cv_chain
+    cv_wrap
+    cv_map
+    cv_grep
+) ] );
+
+our @EXPORT_OK = @{ $EXPORT_TAGS{ all } }; 
+
+our $VERSION = '0.01';
+
+sub cv {
+    my $res = shift;
+
+    return $res 
+        if defined $res and ref( $res ) eq 'AnyEvent::CondVar'; 
+
+    return cv( $res->() )
+        if defined $res and ref( $res ) eq 'CODE';
+
+    my $cv = AnyEvent::condvar;
+    $cv->send( $res, @_ );
+    $cv;
+}
+
+sub cv_and {
+    my @args = @_;
+
+    my @result;
+    my $cv = AnyEvent::condvar;
+    
+    $cv->begin( sub {
+        $cv->send( map{ @$_ } @result ); 
+    });
+
+    for my $i ( 0..$#args ){
+        $cv->begin;
+        cv( $args[$i] )->cb( sub {
+            $result[$i] = [ shift->recv ];
+            $cv->end;
+        });
+    }
+
+    $cv->end;
+    $cv;
+}
+
+sub cv_or {
+    my @args = @_;
+
+    my $done = 0;
+    my $cv = AnyEvent::condvar;
+    
+    cv( $_ )->cb( sub {
+        return if $done;
+        $done = 1;
+        $cv->send( shift->recv );
+    }) for ( @args );
+
+    $cv;
+}
+
+sub cv_chain(&@) {
+    my @fns = @_;
+
+    my $cv = AnyEvent->condvar;
+
+    my $fp = undef;
+    foreach my $fn ( reverse @fns ) {
+        my $next = $fp; $fp = sub {
+            cv( $fn->( @_ ) )->cb( sub {
+                defined $next ?
+                    $next->( shift->recv ) :
+                    $cv->send( shift->recv );
+            });  
+        };
+    }
+    $fp->();
+
+    $cv;
+}
+
+sub then(&@) { @_ }
+sub with(&@) { @_ }
+
+sub cv_wrap(&$) {
+    my ( $fn, $cv ) = @_;
+
+    my $cv_wrap = AnyEvent::condvar;
+
+    cv( $cv )->cb( sub {
+        $cv_wrap->send( 
+            $fn->( shift->recv ) );
+    });
+
+    $cv_wrap;
+}
+
+sub cv_map(&@) {
+    my ( $fn, @list ) = @_;
+    cv_and( map { $fn->() } @list );
+}
+
+sub cv_grep(&@) {
+    my ( $fn, @list ) = @_;
+    cv_chain {
+        cv_map {
+            my $elem = $_;
+            cv_wrap { [ shift, $elem ] } $fn->();
+        } @list;
+    } with {
+        map { $_->[1] } grep { $_->[0] } @_;
+    };
+}
+
+sub cv_timer {
+    my ( $after, $cb ) = @_;
+    my $cv = AnyEvent::condvar;
+    my $w; $w = AnyEvent->timer(
+        after => $after,
+        cb => sub {
+            $cv->send( defined $cb ? $cb->() : '' );
+            undef $w;
+        },
+    );
+    $cv;
+}
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Async::Util - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Async::Util;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Async::Util, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Nautilus, E<lt>nautilus@E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Nautilus
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.14.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
---
+++ t/CondVar-Util.t
@@ -0,0 +1,113 @@
+use strict;
+use warnings;
+
+use Test::More; 
+use Data::Dumper;
+
+BEGIN { use_ok( 'CondVar::Util', qw( :all ) ) };
+
+ok_cv(
+    cv(2),
+    [ 2 ],
+    'cv creates a condvar when passed a scalar'
+);
+
+ok_cv(
+    cv( 1, 2, 3 ),
+    [ 1, 2, 3 ],
+    'cv creates a condvar that returns a list of all its arguments'
+);
+
+ok_cv(
+    cv( sub{ sub{ ( 1, 2, 3 ) } } ),
+    [ 1, 2, 3 ],
+    'cv recursively evalutes functions passed as argument'
+);
+
+ok_cv(
+    cv( cv(2) ),
+    [ 2 ],
+    'cv returns its argument if its a condvar'
+);
+
+ok_cv(
+    cv_timer( 0.1 => sub{2} ),
+    [ 2 ],
+    'cv_timer evalutes its callback and returns it'
+);
+
+ok_cv(
+    cv_and(
+        cv(1),
+        cv_timer( 0.2 => sub{2} ),
+        cv(3),
+        cv_timer( 0.1 => sub{4} ),
+        cv(5),
+    ),
+    [ 1, 2, 3, 4, 5 ],
+    'cv_and returns an ordered list of all the condvar values'
+);
+
+ok_cv(
+    cv_or(
+        cv_timer( 0.4 => sub{1} ),
+        cv_timer( 0.3 => sub{2} ),
+        cv_timer( 0.2 => sub{3} ),
+        cv_timer( 0.1 => sub{4} ),
+    ),
+    [ 4 ],
+    'cv_or returns the value of the first condvar to complete'
+);
+
+ok_cv(
+    (cv_chain {
+        cv_timer( 0.1 => sub{ 1 } );
+    } with {
+        cv( @_, 2 );
+    } with {
+        my @args = @_;
+        cv_timer( 0.1 => sub{ ( @args, 3 ) } );
+    }),
+    [ 1, 2, 3 ],
+    'cv_chain chains the result of a condvar with the next function'
+);
+
+ok_cv(
+    ( cv_wrap { ( @_, 3 ) } cv( 1, 2 ) ),
+    [ 1, 2, 3 ],
+    'cv_wrap maps the result of a condvar into another one'
+);
+
+ok_cv(
+    ( cv_map {
+        my $elem = $_;
+        cv_timer( $elem => sub{ $elem } );
+    } ( 0.3, 0.2, 0.1 ) ),
+    [ 0.3, 0.2, 0.1 ],
+    'cv_map maps elements to condvars and cv_and them together'
+);
+
+ok_cv(
+    ( cv_grep {
+        my $elem = $_;
+        cv_timer( $elem => sub{ int( $elem*10 ) % 2 } ); 
+    } ( 0.4, 0.3, 0.2, 0.1 ) ),
+    [ 0.3, 0.1 ],
+    'cv_grep maps elements to condvars the return filters'
+);
+
+ok_cv(
+    ( cv_grep {
+        my $elem = $_;
+        cv_timer( 0.1 => sub{ not defined $elem } ); 
+    } ( 1, undef, 2, undef ) ),
+    [ undef, undef ],
+    'cv_grep must work with undef filters'
+);
+
+done_testing();
+
+sub ok_cv {
+    my ( $cv, $res, $name ) = @_;
+    is_deeply( [ $cv->recv ], $res, $name );
+}
