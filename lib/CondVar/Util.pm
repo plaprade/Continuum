ae673420c9aaf4995f30c01cae39a636309b38cc
ciphermonk <ciphermonk@altera-vita.com>
2012-12-15 20:19:43 +0700

    Adding pod documentation for github

--- lib/CondVar/Util.pm
+++ lib/CondVar/Util.pm
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 
 use AnyEvent;
-use Carp;
 
 require Exporter;
 
@@ -146,54 +145,3 @@ sub cv_timer {
 }
 
 1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
-
-=head1 NAME
-
-Async::Util - Perl extension for blah blah blah
-
-=head1 SYNOPSIS
-
-  use Async::Util;
-  blah blah blah
-
-=head1 DESCRIPTION
-
-Stub documentation for Async::Util, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
-
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
-=head1 AUTHOR
-
-Nautilus, E<lt>nautilus@E<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2012 by Nautilus
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.14.2 or,
-at your option, any later version of Perl 5 you may have available.
-
-
-=cut
